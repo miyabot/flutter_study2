e5a6b8fdd98687f9359b0df79faf865a994b45da
miyabot <kmiyamoto@st.kobedenshi.ac.jp>
2025-06-23 10:19:24 +0900

    6/23

--- x_trace/lib/main.dart
+++ x_trace/lib/main.dart
@@ -22,7 +22,10 @@ class TimelinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, //bodyの背景色
+
       appBar: AppBar(
+        shape: Border(bottom: BorderSide(color: Colors.grey)),
         backgroundColor: Colors.white, //AppBarの背景色
         title: Center(
           //ネットワーク上の画像を取得
@@ -33,13 +36,62 @@ class TimelinePage extends StatelessWidget {
           ),
         ),
       ),
+
+      //メインコンテンツ部分
+      //ListView.builder:スクロール可能なリスト(可変式)
+      body:ListView.builder(
+        itemCount: 10, //要素数
+
+        //要素ごとの処理
+        itemBuilder: (context,index){
+          
+          //Padding:内側に余白を作成
+          return Padding(
+            //全方向に余白を作成
+            padding: const EdgeInsets.all(12.0),
+
+            //横に並べる
+            child: Row(
+              children: [
+                //丸形アイコン
+                CircleAvatar(
+                  //ネットワーク上の画像をアイコンに設定
+                  backgroundImage: NetworkImage('https://news.mynavi.jp/article/20240820-3008612/images/102.jpg'),
+                ),
+                SizedBox(width: 12,),
+                Column(
+                  //mainAxisAlignment:縦軸の配置
+                  //crossAxisAlignment:横軸の配置
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '宮本 @miyamoto_dayo',
+                      style: TextStyle(fontWeight: FontWeight.bold)
+                    ),
+                    SizedBox(height:4),
+                    Text('xのタイムラインをトレースするためのプロジェクト'),
+                  ],
+                )
+              ],
+            ),
+          );
+        }
+      ),
+
       //画面下部に表示するバー
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white, //bottomNavの背景色
+
+        //４つ以上並べる場合
+        type:BottomNavigationBarType.fixed,
+
         //各要素
         items:[
           BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
           BottomNavigationBarItem(icon: Icon(Icons.search),label: 'search'),
-          BottomNavigationBarItem(icon: Icon(Icons.people),label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.people_outline),label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_none_outlined),label:'notifications'),
+          BottomNavigationBarItem(icon: Icon(Icons.mail_outline),label:'mail'),
         ] 
       ),
     );
