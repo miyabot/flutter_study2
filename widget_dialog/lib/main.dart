385d55751f27c07d9138db7a8e451a22251e7137
miyabot <kmiyamoto@st.kobedenshi.ac.jp>
2025-09-03 15:08:27 +0900

    9/3

--- widget_dialog/lib/main.dart
+++ widget_dialog/lib/main.dart
@@ -42,7 +42,39 @@ class _DialogPageState extends State<DialogPage> {
                     title: Center(
                       child: Text('アカウント削除',style:TextStyle(fontWeight:FontWeight.bold))
                     ),
+                    //コンテンツ部分
+                    content: Column(
+                      //内容に合わせて最小限の高さになる
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('削除すると以下の情報がすべて失われます'),
+                        SizedBox(height: 24), //縦24ピクセル分の透明の箱
+                        //見た目を変える事の出来る箱
+                        Container(
+                          //見た目の変更
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(55, 158, 158, 158),
+                            borderRadius: BorderRadius.circular(10), //角度
+                          ),
 
+                          //余白の作成
+                          padding: EdgeInsets.only(left: 12),
+                          height: 150, //コンテナの高さ
+                          width: 200, //コンテナの幅
+
+                          child:Column(
+                            mainAxisAlignment: MainAxisAlignment.center,  //中央
+                            crossAxisAlignment: CrossAxisAlignment.start, //左端
+                            children: [
+                              Text('・プロフィール情報'),
+                              Text('・メッセージ'),
+                              Text('・お気に入り'),
+                              Text('・フォロー/フォロワー'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
                 ); 
               }
             );
